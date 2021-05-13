8fe2bad01b875d8e12663d6a665bd1d5adc63c54
Sachin rajput <sachinrajputlive@gmail.com>
2021-05-14 01:26:32 +0530

    deploy my react app to git hub pages

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import NavigationBar from './navigationBar'
+import CssBaseLine from '@material-ui/core/CssBaseline'
+
+function App() {
+  return (
+    <div className='App'>
+      <CssBaseLine />
+      <NavigationBar />
+      <h1>hello i am sachin</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/navigationBar.js
@@ -0,0 +1,46 @@
+import {
+  Toolbar,
+  Typography,
+  Container,
+  Box,
+  Link,
+  makeStyles,
+} from '@material-ui/core'
+import { findByLabelText } from '@testing-library/dom'
+import React from 'react'
+
+const useStyles = makeStyles((theme) => ({
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  menuOption: {
+    padding: theme.spacing(1),
+    background: '#300',
+    margin: '1px',
+    color: '#fff',
+  }
+}
+)),
+
+export default function NavigationBar() {
+  const classes = useStyles()
+  return (
+    <Container>
+      <Toolbar className={classes.toolbar}>
+        <Typography>Hello</Typography>
+        <Box className={classes.menuBox}>
+          {['home', 'courses', 'sign up'].map((menuOption) => (
+            <Link
+              component='button'
+              variant='body1'
+              className={classes.menuOption}
+            >
+              {menuOption.toUpperCase()}
+            </Link>
+          ))}
+        </Box>
+      </Toolbar>
+    </Container>
+  )
+}
